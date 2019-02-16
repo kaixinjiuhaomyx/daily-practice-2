b65287e50fe86e36f34318048cedd29ccb52fc20
kaixinjiuhaomyx <1416191825@qq.com>
2019-02-16 16:20:57 +0800

    vue.cli

--- dox_app/src/views/music/Music.vue
+++ dox_app/src/views/music/Music.vue
@@ -1,20 +1,35 @@
 <template>
     <div>
-        <ul>
-            <li>
-                <div></div>
-            </li>
-
-        </ul>
+        <img v-for="(obj,index) in music" :key="index" :src="obj.bg" />
     </div>
 </template>
 
 <script>
+    import Axios from 'axios';
     export default {
-        
+        data(){
+            return{
+                music:[]
+            }
+        },
+        created(){
+            // 找接口
+            Axios.get('./data/musiclist.json')
+            .then(
+                (result)=>{
+                    // console.log(result);
+                    this.music = result.data.albums;
+                }
+            )
+            .catch(
+
+            )
+        }
     }
 </script>
 
 <style scoped>
-
+    img{
+        width: 50%;
+    }
 </style>
