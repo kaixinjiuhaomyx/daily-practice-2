0bd29f325a44db44ff3f9408c2cece760b4774a4
kaixinjiuhaomyx <1416191825@qq.com>
2019-05-25 23:37:16 +0800

    node

--- node/1.text.js
+++ node/1.text.js
@@ -12,26 +12,26 @@
 // // 有return的是函数 没return的是过程
 
 
-// var aa = function(){
-//     // 函数表达式
-//     console.log(123);
-// }
+var aa = function(){
+    // 函数表达式
+    console.log(123);
+}
 
-// function aa(){
-//     console.log(456);
-// }
-// aa();
+function aa(){
+    console.log(456);
+}
+aa();
 // 解释器、预编译、
 // v8处理代码  1.预编译  先看var 给aa=undefined  再 function aa(){}加载内存（代码此时在内存）
 //             2. 代码执行 var aa = undefined-->function aa(){} 覆盖掉了上一个aa（）；
 //             3. aa()
 
 
-var start = new Date();
+// var start = new Date();
 
-setTimeout(function(){
-    var end = new Date();
-    console.log('Time waste is ',end-start,'ms');
-},500);
+// setTimeout(function(){
+//     var end = new Date();
+//     console.log('Time waste is ',end-start,'ms');
+// },500);
 
-while(new Date-start < 1000){}
+// while(new Date-start < 1000){}
