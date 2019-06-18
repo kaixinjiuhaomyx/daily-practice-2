d5dabac0881fca5286ec5575617698f176664b2e
kaixinjiuhaomyx <1416191825@qq.com>
2019-06-18 22:10:57 +0800

    app

--- vue-app/src/views/movie/Movie.vue
+++ vue-app/src/views/movie/Movie.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>电影</div>
+    <div>
+        <ul>
+            <li>
+                <img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=138126325,1485620701&fm=85&app=63&f=JPEG?w=121&h=75&s=7FAB2EC3909A35D01E299C1A030010D2" alt="">
+                <div>
+                    <h3>流浪地球</h3>
+                    <p>屈楚萧</p>
+                    <p>1005152已观看</p>
+                    <p>年份</p>
+                    <p>科幻</p>
+                </div>
+            </li>
+        </ul>
+
+    </div>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style>
+<style scoped>
 
 </style>
