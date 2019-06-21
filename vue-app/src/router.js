f978e4b58c14ca10d1947b071bb9ce25ec51acee
kaixinjiuhaomyx <1416191825@qq.com>
2019-06-21 21:53:19 +0800

    app

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -5,6 +5,7 @@ import Music from './views/music/Music.vue'
 import Book from './views/book/Book.vue'
 import Photo from './views/photo/Photo.vue'
 import MusicList from './views/music/MusicList.vue'
+import Slide from './components/Slide.vue'
 
 Vue.use(Router);
 
@@ -31,7 +32,10 @@ export default new Router({
     },{
       path:'/musiclist',
       component: MusicList
-    }
+    },{
+      path:'/slide',
+      component: Slide
+    },
     
   ],
 });
