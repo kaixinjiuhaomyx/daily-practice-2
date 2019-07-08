7eb47cfb74e8416cf7a448b8edfca1a539bd0d6c
kaixinjiuhaomyx <1416191825@qq.com>
2019-07-08 20:35:54 +0800

    安装mysql

--- node1/expressll/controllers/user.js
+++ node1/expressll/controllers/user.js
@@ -1,4 +1,28 @@
 // 注册
+
+
+
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'newblog'
+// });
+ 
+
+
+
 exports.reg = function(req,res,next){
     res,render("reg.ejs")
+
+    // connection.connect();
+ 
+    // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    // if (error) throw error;
+    // console.log('The solution is: ', results[0].solution);
+    // connection.end();
+    // });
+    
+   
 }
