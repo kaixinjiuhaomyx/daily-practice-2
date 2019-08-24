c2228af37b7c3abf2264222c2ee0f8ace702e537
kaixinjiuhaomyx <1416191825@qq.com>
2019-08-24 14:54:45 +0800

    数据结构

---
+++ 面试/数据结构/排序/快排.js
@@ -0,0 +1,21 @@
+function quickSort( arr ){
+    if ( arr.length == 0) {
+        return [];
+    }
+    var left = [];
+    var right = [];
+    var pivot = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push( arr[i] );
+        } else {
+            right.push( arr[i] );
+        }
+    }
+    return quickSort( left ).concat( pivot, quickSort( right ));
+}
+
+var arr1 = [72,1,68,95,75,54,58,10,35,6,28,45,69,13,88,99,24,28,30,31,78,2,77,82,72];
+
+var arr = quickSort( arr1 );
+console.log(arr);
