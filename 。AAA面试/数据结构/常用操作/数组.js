8b65d3844fb73dc4ba09441a418afa850d7c2652
kaixinjiuhaomyx <1416191825@qq.com>
2019-08-24 20:45:12 +0800

    aa

---
+++ 。AAA面试/数据结构/常用操作/数组.js
@@ -0,0 +1,31 @@
+// 会改变数组自身的方法
+// var arr = [1,2,3,4,5,6,7,8];
+
+// var newArr = arr.pop(); //8  [ 1, 2, 3, 4, 5, 6, 7 ]
+
+// var newArr = arr.push('a');  // 9  [ 1, 2, 3, 4, 5, 6, 7, 8, 'a' ]
+
+// var newArr = arr.shift();  // 1 [ 2, 3, 4, 5, 6, 7, 8 ]
+
+// var newArr = arr.unshift('a');  // 9  [ 'a', 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+// var newArr = arr.reverse(); // [ 8, 7, 6, 5, 4, 3, 2, 1 ]  [ 8, 7, 6, 5, 4, 3, 2, 1 ]
+
+// var newArr = arr.sort();  // 排序 [1,2,3,4,5,6,7,8]  [1,2,3,4,5,6,7,8]
+
+// var newArr = arr.splice(3,2,'a'); // （开始 删 加） [ 4, 5 ]  [ 1, 2, 3, 'a', 6, 7, 8 ]
+
+// console.log(newArr);
+// console.log(arr);
+
+
+
+// 不改变数组自身的方法
+var arr1 = [1,2,3];
+var arr2 = [4,5,6];
+
+
+
+
+
+
